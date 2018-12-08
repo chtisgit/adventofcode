1f93094c75479371fecf1a4510c0c54f2594e70b
Christian Fiedler <fdlr.christian@gmail.com>
2018-12-08 17:55:46 +0100

    Finish #8

--- 2018/cpp/8.cpp
+++ 2018/cpp/8.cpp
@@ -1,27 +1,83 @@
 #include <cstdio>
+#include <memory>
+#include <numeric>
 #include <set>
 #include <vector>
 
-template<typename RandomAccessIterator>
-std::pair<RandomAccessIterator,int> sumMeta(RandomAccessIterator begin, RandomAccessIterator end)
-{
-	if(begin == end)
-		return std::make_pair(begin, 0);
-	auto children = *begin++;
-	auto metadata = *begin++;
-	auto sum = 0;
+struct Tree {
+	struct Node {
+		Tree &tree;
+		std::vector<int> children;
+		std::vector<int> metadata;
 
-	printf("mychildren:%d metadata:%d\n",children, metadata);
-	for(;children != 0; --children){
-		auto p = sumMeta(begin, end - metadata);
-		begin = p.first;
-		sum += p.second;
+		Node(Tree &tree) : tree(tree)
+		{
+		}
+
+		int sum() const
+		{
+			int sum = std::accumulate(metadata.begin(),
+			                          metadata.end(), 0);
+			for (const auto &child : children) {
+				sum += tree.nodes[child].sum();
+			}
+			return sum;
+		}
+
+		int value() const
+		{
+			if (children.empty()) {
+				return std::accumulate(metadata.begin(),
+				                       metadata.end(), 0);
+			}
+			int sum = 0;
+			for (auto child : metadata) {
+				if (child >= 1 && child <= children.size()) {
+					sum += tree.nodes[children[child - 1]]
+					           .value();
+				}
+			}
+			return sum;
+		}
+	};
+
+	std::vector<Node> nodes;
+
+	auto sum()
+	{
+		return nodes[0].sum();
 	}
-	for(;metadata != 0; --metadata){
-		sum += *begin++;
+
+	auto value()
+	{
+		return nodes[0].value();
 	}
-	return std::make_pair(begin, sum);
-}
+
+	template <typename RandomAccessIterator>
+	std::pair<RandomAccessIterator, int>
+	buildTree(RandomAccessIterator begin, RandomAccessIterator end)
+	{
+		auto children = *begin++;
+		auto metadata = *begin++;
+
+		auto index = nodes.size();
+		nodes.emplace_back(*this);
+
+		for (; children != 0; --children) {
+			auto p = buildTree(begin, end - metadata);
+			begin = p.first;
+			nodes[index].children.push_back(p.second);
+		}
+		for (; metadata != 0; --metadata)
+			nodes[index].metadata.push_back(*begin++);
+		return make_pair(begin, index);
+	}
+
+	Tree(std::vector<int> &input)
+	{
+		buildTree(input.cbegin(), input.cend());
+	}
+};
 
 int main()
 {
@@ -32,9 +88,10 @@ int main()
 			break;
 		input.push_back(a);
 	}
-	
-	auto p = sumMeta(input.cbegin(), input.cend());
-	printf("metadata sum: %d\n", p.second);
+	Tree tree(input);
+
+	printf("metadata sum: %d\n", tree.sum());
+	printf("value of root: %d\n", tree.value());
 
 	return 0;
 }
