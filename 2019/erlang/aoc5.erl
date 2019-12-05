448faae70ebbb7b6ed2bf6b3c264258e55c4b845
Christian Fiedler <fdlr.christian@gmail.com>
2019-12-05 15:45:06 +0100

    Use array instead of list in program 5

--- 2019/erlang/aoc5.erl
+++ 2019/erlang/aoc5.erl
@@ -1,10 +1,13 @@
 -module(aoc5).
 -export([main/0]).
 
-d(I, L, 1) -> lists:nth(I+1,L);
-d(I, L, 0) -> lists:nth(lists:nth(I+1, L)+1, L).
+b2i(true) -> 1;
+b2i(_) -> 0.
 
-d(I, L) -> d(I, L, 1).
+d(I, RAM, 1) -> array:get(I, RAM);
+d(I, RAM, 0) -> array:get(array:get(I, RAM), RAM).
+
+d(I, RAM) -> d(I, RAM, 1).
 
 r(RAM, PC, Amode, Bmode) ->
     {d(PC+1, RAM, Amode), d(PC+2, RAM, Bmode), d(PC+3, RAM)}.
@@ -12,8 +15,7 @@ r(RAM, PC, Amode, Bmode) ->
 vmexec({3,_,_}, RAM, PC) ->
     I = util:getInputInt(),
     StAddr = d(PC+1, RAM),
-    io:format("StAddr: ~p  I=~p~n", [StAddr,I]),
-    {lists:sublist(RAM,StAddr) ++ [I] ++ lists:nthtail(StAddr+1,RAM), PC + 2};
+    {array:set(StAddr, I, RAM), PC + 2};
 vmexec({4,_,Bmode}, RAM, PC) ->
     LdVal = d(PC+1, RAM, Bmode),
     io:format("~p~n", [LdVal]),
@@ -32,18 +34,15 @@ vmexec({99,_,_}, RAM, _) ->
     {RAM, ended};
 vmexec({Opcode, Amode, Bmode}, RAM, PC) ->
     {A, B, AddrC} = r(RAM, PC, Amode, Bmode),
-    io:format("Op: ~p A(Mode=~p): ~p B(Mode=~p): ~p AddrC: ~p~n", [Opcode,Amode,A,Bmode,B,AddrC]),
     if Opcode =:= 1 -> Res = A+B;
        Opcode =:= 2 -> Res = A*B;
-       Opcode =:= 7 andalso A < B -> Res = 1;
-       Opcode =:= 7 -> Res = 0;
-       Opcode =:= 8 andalso A =:= B -> Res = 1;
-       Opcode =:= 8 -> Res = 0;
+       Opcode =:= 7 -> Res = b2i(A < B);
+       Opcode =:= 8 -> Res = b2i(A =:= B);
        true -> Res = error
     end,
-    {lists:sublist(RAM,AddrC) ++ [Res] ++ lists:nthtail(AddrC+1,RAM), PC + 4};
+    {array:set(AddrC, Res, RAM), PC + 4};
 vmexec(Op,RAM,_) ->
-    io:format("Bad? Opcode: ~p~n", [Op]),
+    io:format("error: bad opcode: ~p~n", [Op]),
     {RAM, error}.
 
 vm(RAM, PC) when is_atom(PC) -> {RAM,PC};
@@ -54,5 +53,5 @@ vm(RAM, PC) ->
 
 main() ->
     RAM = util:getInputCommaInts(),
-    vm(RAM, 0).
-
+    {_, PC} = vm(array:fix(array:from_list(RAM)), 0),
+    PC.
