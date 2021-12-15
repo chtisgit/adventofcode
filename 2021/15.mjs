e5c5d84726840f5cae66103f5093b560fba5ecba
Christian Fiedler <fdlr.christian@gmail.com>
2021-12-15 20:04:51 +0100

    2021: optimize program 15

--- 2021/15.mjs
+++ 2021/15.mjs
@@ -1,11 +1,30 @@
 
 import { lines } from './input.mjs';
 
-function* coords(width, height) {
-    for (let y = 0; y !== height; y++)
-        for (let x = 0; x !== width; x++)
-            yield {x,y};
-}
+const arr2D = (w,h,val) => Array.from({length:h}, () => Array.from({length:w}, () => val));
+
+const findIndex = (arr, val, lessThan) => {
+    let low = 0, high = arr.length;
+    while (low < high) {
+        const mid = (low + high) >>> 1;
+        if (lessThan(arr[mid], val)) {
+            low = mid + 1;
+        } else {
+            high = mid
+        }
+    }
+    return low;
+};
+
+const insertAt = (arr, val, lessThan) => {
+    const position = findIndex(arr, val, lessThan);
+    for (let i = position; i !== arr.length; i++){
+        const tmp = arr[i];
+        arr[i] = val;
+        val = tmp;
+    };
+    arr.push(val);
+};
 
 const shortest = (target, pred) => {
     const path = [target];
@@ -16,27 +35,23 @@ const shortest = (target, pred) => {
     return path;
 }
 
-const arr2D = (w,h,val) => Array.from({length:h}, () => Array.from({length:w}, () => val));
-
 const dijkstra = (map, source, target, risk) => {
     const w = map[0].length, h = map.length;
-    const Q = [...coords(w,h)];
-    const neighbors = ({x,y}) => 
-        [{x:x,y:y-1},{x:x-1,y:y},{x:x+1,y:y},{x:x,y:y+1}]
-        .filter((pos) => risk(pos) < 10);
+    const neighbors = ({x,y}) => [{x:x,y:y-1},{x:x-1,y:y},{x:x+1,y:y},{x:x,y:y+1}].filter((pos) => risk(pos) < 10);
     const dist = arr2D(w,h,Infinity)
     const pred = arr2D(w,h,null);
-    
-    dist[source.y][source.x] = 0;
+	const wasinq = arr2D(w,h,false);
 
     const done = Array.from({length:w*h}, () => false);
     
-    while(Q.length !== 0) {
-        const u = Q.reduce((acc, {x,y}) => acc === null || dist[y][x] < dist[acc.y][acc.x] ? {x,y} : acc, null);
-        const ri = Q.findIndex(({x,y}) => x === u.x && y === u.y);
-        Q.splice(ri, 1);
+    const Q = [source];
+    dist[source.y][source.x] = 0;
+    wasinq[source.y][source.x] = true;
+
+    while(Q.length !== 0 && !done[target.x+target.y*w]) {
+        const u = Q.shift();
         done[u.x+u.y*w] = true;
-    
+
         for (let v of neighbors(u)) {
             if (!done[v.x+v.y*w]) {
                 const alt = dist[u.y][u.x] + risk(v);
@@ -45,8 +60,12 @@ const dijkstra = (map, source, target, risk) => {
                     pred[v.y][v.x] = u;
                 }
             }
+
+			if(!wasinq[v.y][v.x]){		
+                insertAt(Q, v, (a,b) => dist[a.y][a.x] < dist[b.y][b.x]);
+				wasinq[v.y][v.x] = true;
+			}
         }
-        console.log('Q.length: ', Q.length);
     }
 
     return shortest(target, pred);
@@ -67,14 +86,21 @@ const fullmap = (map, w, h) => {
     return nmap;
 }
 
+const time = (f) => {
+    const s = Date.now();
+    const result = f();
+    const time = Date.now() - s;
+    return { result, time };
+}
+
 lines().then((data) => {
     const w = data[0].length, h = data.length;
     const risk = ({x,y}) => data[y]?.[x] ? +data[y][x] : 10;
-    const part1 = dijkstra(data, {x:0,y:0}, {x:w-1,y:h-1}, risk).map((pos) => risk(pos)).reduce((acc,val) => acc+val, 0) - risk({x:0,y:0});
-    console.log('part 1:', part1);
+    const part1 = time(() => dijkstra(data, {x:0,y:0}, {x:w-1,y:h-1}, risk).map((pos) => risk(pos)).reduce((acc,val) => acc+val, 0) - risk({x:0,y:0}));
+    console.log('part 1:', part1.result, ` (elapsed time ${part1.time} ms)`);
     
     const data2 = fullmap(data, w, h);
     const risk2 = ({x,y}) => data2[y]?.[x] ? +data2[y][x] : 10;
-    const part2 = dijkstra(data2, {x:0,y:0}, {x:5*w-1,y:5*h-1}, risk2).map((pos) => risk2(pos)).reduce((acc,val) => acc+val, 0) - risk({x:0,y:0});
-    console.log('part 2:', part2);
+    const part2 = time(() => dijkstra(data2, {x:0,y:0}, {x:5*w-1,y:5*h-1}, risk2).map((pos) => risk2(pos)).reduce((acc,val) => acc+val, 0) - risk({x:0,y:0}));
+    console.log('part 2:', part2.result, ` (elapsed time ${part2.time} ms)`);
 });
