8c7214c34e8dde366e9bb1829995f6ddc8fcd18c
Christian Fiedler <fdlr.christian@gmail.com>
2021-12-18 20:07:31 +0100

    2021: program 18: save a few lines & replace deprecated method
    apparently substr is deprecated, so it was replaced with slice.
    Save a few lines here and there in the code to make it look
    less complex.

--- 2021/18.mjs
+++ 2021/18.mjs
@@ -1,63 +1,50 @@
-
 import { lines } from './input.mjs';
 
 const reduce = (acc) => {
     let depth = 0;
     for (let i = 0; i !== acc.length; i++) {
-        let ch = acc[i];
-        let num = null;
-    
-        if (ch === '[') depth++;
-        if (ch === ']') depth--;
+        if (acc[i] === '[') depth++;
+        if (acc[i] === ']') depth--;
             
         if (depth > 4) {
-            const match = /^\[([0-9]+),([0-9]+)\]/.exec(acc.substr(i));
+            const match = /^\[([0-9]+),([0-9]+)\]/.exec(acc.slice(i));
             if (match) {
-                acc = acc.substr(0,i) + '0' + acc.substr(i+match[0].length);
+                acc = acc.slice(0,i) + '0' + acc.slice(i+match[0].length);
 //                console.log('explode ', match[0]);
-                
-                let left = acc.substr(0, i-1).match(/[0-9]+/g);
-                left = left?.[left?.length-1];
-                const right = acc.substr(i+1).match(/[0-9]+/g)?.[0];
+                const left = acc.slice(0, i-1).match(/[0-9]+/g)?.pop();
+                const right = acc.slice(i+1).match(/[0-9]+/g)?.[0];
                 
                 if (right) {
                     const j = acc.indexOf(right, i+1);
-                    acc = acc.substr(0, j) + ((+match[2])+(+right)) + acc.substr(j+right.length); 
+                    acc = acc.slice(0, j) + ((+match[2])+(+right)) + acc.slice(j+right.length); 
                 }
                 
                 if (left) {
-                    const j = acc.substr(0, i-1).lastIndexOf(left);
-                    acc = acc.substr(0, j) + ((+match[1])+(+left)) + acc.substr(j+left.length);
+                    const j = acc.slice(0, i-1).lastIndexOf(left);
+                    acc = acc.slice(0, j) + ((+match[1])+(+left)) + acc.slice(j+left.length);
                 }
                 
-                return acc;
+                return [acc, true];
             }      
         }
     }
 
     for (let i = 0; i !== acc.length; i++) {
-        let ch = acc[i];
-        let num = null;
-        if (/[0-9]/.test(ch)) num = /^[0-9]+/.exec(acc.substr(i))[0];
-            
-        if (num !== null && +num >= 10) {
+        const num = /^[0-9]+/.exec(acc.slice(i))?.[0];   
+        if (num !== undefined && +num >= 10) {
 //            console.log('split ', num);
-            acc = acc.substr(0,i) + `[${Math.floor((+num)/2)},${Math.ceil((+num)/2)}]` + acc.substr(i+num.length);
-            return acc;
+            acc = acc.slice(0,i) + `[${Math.floor((+num)/2)},${Math.ceil((+num)/2)}]` + acc.slice(i+num.length);
+            return [acc, true];
         }
     }
 
-    return null;
+    return [acc, false];
 }
 
 const add = (a,b) => {
-    let acc = `[${a},${b}]`;
-    do {
-        a = acc;
-        acc = reduce(acc);
-    } while(acc !== null);
-    
-    return a;
+    let acc = `[${a},${b}]`, reduced = true;
+    while (reduced) { [acc, reduced] = reduce(acc); }
+    return acc;
 }
 
 const magn = (arr) => typeof arr === 'number' ? arr : 3*magn(arr[0])+2*magn(arr[1]);
